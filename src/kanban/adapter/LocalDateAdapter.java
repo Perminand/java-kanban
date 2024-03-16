95bdaad62c60ea237d910962a37a1a329888585e
Андрей <Perminand@yandex.ru>
2024-03-16 07:34:20 +0300

    feet: Убрать переменные

--- src/kanban/adapter/LocalDateAdapter.java
+++ src/kanban/adapter/LocalDateAdapter.java
@@ -9,16 +9,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateAdapter extends TypeAdapter<LocalDateTime> {
-    private static final DateTimeFormatter formatterWrite = DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm:ss");
-    private static final DateTimeFormatter formatterReader = DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm:ss");
-
     @Override
     public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
-        jsonWriter.value(localDateTime.format(formatterWrite));
+        jsonWriter.value(localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
     }
 
     @Override
     public LocalDateTime read(JsonReader jsonReader) throws IOException {
-        return LocalDateTime.parse(jsonReader.nextString(), formatterReader);
+        return LocalDateTime.parse(jsonReader.nextString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 }
