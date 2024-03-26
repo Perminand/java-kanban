7d7067478c3c020dc328ca7df194f95d987a4325
Андрей <Perminand@yandex.ru>
2024-03-26 12:15:08 +0300

    refactor: Сделать рефакторинг

--- src/kanban/model/Task.java
+++ src/kanban/model/Task.java
@@ -14,6 +14,7 @@ public class Task {
     protected TypeTask typeTask;
     protected LocalDateTime startTime;
     protected Duration duration;
+
     public Task(String nameTask, String description) {
         this.nameTask = nameTask;
         this.description = description;
